9df4cd8533e82b4f7633390c622ece7d68fc2391
Iyed Gouia <IYEDgouia123>
2024-12-15 17:53:56 +0100

    initial commit

---
+++ techbyte-storefront/src/modules/home/components/hero/index.tsx
@@ -0,0 +1,32 @@
+import { Github } from "@medusajs/icons"
+import { Button, Heading } from "@medusajs/ui"
+
+const Hero = () => {
+  return (
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+        <span>
+          <Heading
+            level="h1"
+            className="text-3xl leading-10 text-ui-fg-base font-normal"
+          >
+            TECHBYTE
+          </Heading>
+          <Heading
+            level="h2"
+            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+          >
+            Your one-stop shop for all things tech
+          </Heading>
+        </span>
+        <a
+          href="https://github.com/medusajs/nextjs-starter-medusa"
+          target="_blank"
+        >
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
